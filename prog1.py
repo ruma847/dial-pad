d01da6c14a13a8c65a68ea6494305edb6cf74c54
ruma847 <rumaambarish@gmail.com>
2025-08-22 22:02:44 +0530

    Add files via upload

---
+++ prog1.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+root.title('Number Pad')
+root.geometry('250x300')
+
+nums = [[9, 8, 7], [6, 5, 4], [3, 2, 1,], ['#', 0, '*']]
+
+for i in range(4):
+    root.columnconfigure(i, weight=1, minsize=75)
+    root.rowconfigure(i, weight=1, minsize=50)
+
+for i in  range(4):
+    for j in range(3):
+        frame = Frame(
+            master=root,
+            relief=SUNKEN,
+            borderwidth=1,
+            bg="#14a2e8"
+        ) 
+        frame.grid(row=i, column=j, sticky="nsew")
+
+        label = Label(master=frame, text=nums[i][j], bg='#0D41ED', font=('Arial', 18))
+        label.pack(expand=True, fill='both', padx=5, pady=5)
+
+root.mainloop()
+
